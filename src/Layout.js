65fe5db61f51043a28cf01bc3c17bfbbd2495096
jayant.singh <jayant.singh@webaccessglobal.com>
2021-08-04 19:12:59 +0530

    Web App initial commit

--- src/Layout.js
+++ src/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Header from './Header'
 import Dashboard from './Dashboard';
-import {Route, Redirect} from "react-router-dom";
+import {Route, Redirect, Switch} from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,9 +19,11 @@ export default function Layout() {
     <div className={classes.root}>
       <Header/>
       <main className={classes.header}>
+      <Switch>
         <Route path="/foods/:id" exact children={<Dashboard />} />
         <Route path="/foods" exact component={Dashboard} />
-        {/* <Route path="/" render={() => <Redirect to="/foods" />} /> */}
+        <Route path="/" render={() => <Redirect to="/foods" />} />
+      </Switch>
       </main>
     </div>
   );
